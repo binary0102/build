1c0387639062aaf5dad32e14f41f1ba9e81a94de
binary0102 <40595269+binary0102@users.noreply.github.com>
2019-05-10 15:50:35 +0700

    Add files via upload

---
+++ t/build/precache-manifest.91efd69783a6744b49836a8b3a25ec7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4958441c95e08162940c98c0ebd940d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de059f834f312120d786",
+    "url": "/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "659f24920c5d487f9106",
+    "url": "/static/css/main.b351a92a.chunk.css"
+  },
+  {
+    "revision": "de059f834f312120d786",
+    "url": "/static/js/2.ae012e7b.chunk.js"
+  },
+  {
+    "revision": "659f24920c5d487f9106",
+    "url": "/static/js/main.d1bd1619.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "61c9551bfa98a6c7bf88e9c36a58abc1",
+    "url": "/static/media/logo.61c9551b.jpeg"
+  }
+]);
---
+++ t1/build/precache-manifest.631e1eefb422ca7bfceddcbc7724dc00.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b9493100871cfc015c94a43e14caca5",
+    "url": "/static/media/banner.9b949310.png"
+  },
+  {
+    "revision": "f584676d23960794fe3d",
+    "url": "/static/css/main.b568ed29.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "126f4e1611b880ce1993",
+    "url": "/static/js/2.73018aac.chunk.js"
+  },
+  {
+    "revision": "5b50ed3f1bdcd5e3b00c7259939fd394",
+    "url": "/static/media/slider_1.5b50ed3f.png"
+  },
+  {
+    "revision": "f584676d23960794fe3d",
+    "url": "/static/js/main.22c2995a.chunk.js"
+  },
+  {
+    "revision": "40575457c03f88df1ba672cc746c8d44",
+    "url": "/static/media/banner2.40575457.png"
+  },
+  {
+    "revision": "647e79658f37f7acc84233da9197e907",
+    "url": "/static/media/banner3.647e7965.png"
+  },
+  {
+    "revision": "79b525991ea4ebdb8a2597e103c01c70",
+    "url": "/static/media/banner4.79b52599.png"
+  },
+  {
+    "revision": "126f4e1611b880ce1993",
+    "url": "/static/css/2.51457e28.chunk.css"
+  },
+  {
+    "revision": "e9878b239198c73413047bdb40c88356",
+    "url": "/index.html"
+  }
+];
